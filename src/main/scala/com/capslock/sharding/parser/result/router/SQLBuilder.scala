83b6bf05df3126dfc1211bb15bbf4c365f32a067
alvin <alvin@liao.com>
2016-08-22 18:24:44 +0800

    add StringToken

--- src/main/scala/com/capslock/sharding/parser/result/router/SQLBuilder.scala
+++ src/main/scala/com/capslock/sharding/parser/result/router/SQLBuilder.scala
@@ -3,7 +3,7 @@ package com.capslock.sharding.parser.result.router
 /**
  * Created by capslock1874.
  */
-class SQLBuilder extends Appendable{
+class SQLBuilder extends Appendable {
     private var segments = List[Any]()
 
 
@@ -12,4 +12,12 @@ class SQLBuilder extends Appendable{
     override def append(csq: CharSequence, start: Int, end: Int): Appendable = ???
 
     override def append(c: Char): Appendable = ???
+}
+
+object SQLBuilder {
+
+    case class StringToken(value: String) {
+        def toToken: String = "[Token(".concat(value).concat(")]")
+    }
+
 }
